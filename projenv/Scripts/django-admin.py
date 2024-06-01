6be4b9f06800649bf486ae345871fe321a7508a3
navaneeth <navaneethprabhupadam@gmail.com>
2024-06-02 03:04:11 +0530

    commit1

---
+++ projenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\NAVAN\OneDrive\Desktop\fullstackfinal\finapproj2\projenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
